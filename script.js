f4e961941d1c4e6a2e39ace05bdc9279ec36f31b
Aleksandrina Velikova <avelikova@tu-sofia.bg>
2023-06-07 12:32:46 +0300

    overlay

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 //button order
- const btn = document.getElementById("btn");
- const form = document.getElementById("form");
- const closeBtn = document.querySelector(".close");
+const btn = document.getElementById("btn");
+const form = document.getElementById("form");
+const overlay = document.getElementById("overlay");
+const closeBtn = document.querySelector(".close");
 
-btn.addEventListener('click', () => {
-
-  if (form.style.display === 'none') { //ako formata e skrita se pokazva
-    form.style.display = 'block';
-   
+btn.addEventListener("click", () => {
+  if (form.style.display === "none") {
+    //ako formata e skrita se pokazva
+    form.style.display = "block";
+    overlay.style.opacity = 1;
   } else {
-    form.style.display = 'none'; //skriva formata
-
+    form.style.display = "none"; //skriva formata
+    overlay.style.opacity = 0;
   }
 });
 
-
 closeBtn.addEventListener("click", () => {
-  form.style.display = 'none'; 
+  form.style.display = "none";
+  overlay.style.opacity = 0;
 });
-
-
 
 //stars
 const starWrapper = document.querySelector(".stars");
@@ -29,10 +28,10 @@ stars.forEach((star, clickedIdx) => {
   star.addEventListener("click", () => {
     starWrapper.classList.add("disabled");
     stars.forEach((otherStar, otherIdx) => {
-      if(otherIdx <= clickedIdx) {
+      if (otherIdx <= clickedIdx) {
         otherStar.classList.add("active");
       }
     });
-    console.log(`star of index ${clickedIdx} was clicked`)
+    console.log(`star of index ${clickedIdx} was clicked`);
   });
 });
