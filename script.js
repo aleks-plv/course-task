aff3d4a9217f7734b19943a791eede737a56722b
Aleksandrina Velikova <avelikova@tu-sofia.bg>
2023-06-19 10:23:49 +0300

    links

--- script.js
+++ script.js
@@ -36,70 +36,70 @@ stars.forEach((star, clickedIdx) => {
 });
 
 // page 1 img
-const imageLink1 = document.getElementById("club5AM");
-const imageLink2 = document.getElementById("devilBook");
-const imageLink3 = document.getElementById("richDad");
-const imageLink4 = document.getElementById("leaders");
-const imageLink5 = document.getElementById("winner");
+// const imageLink1 = document.getElementById("club5AM");
+// const imageLink2 = document.getElementById("devilBook");
+// const imageLink3 = document.getElementById("richDad");
+// const imageLink4 = document.getElementById("leaders");
+// const imageLink5 = document.getElementById("winner");
 
 //img
-imageLink1.addEventListener("click", () => {
-  window.open("5amBook.html", "_blank");
-});
+// imageLink1.addEventListener("click", () => {
+//   window.open("5amBook.html", "_blank");
+// });
 
-imageLink2.addEventListener("click", () => {
-  window.open("devilBook.html", "_blank");
-});
+// imageLink2.addEventListener("click", () => {
+//   window.open("devilBook.html", "_blank");
+// });
 
-imageLink3.addEventListener("click", () => {
-  window.open("richDad.html", "_blank");
-});
+// imageLink3.addEventListener("click", () => {
+//   window.open("richDad.html", "_blank");
+// });
 
-imageLink4.addEventListener("click", () => {
-  window.open("leaders.html", "_blank");
-});
+// imageLink4.addEventListener("click", () => {
+//   window.open("leaders.html", "_blank");
+// });
 
-imageLink5.addEventListener("click", () => {
-  window.open("winner.html", "_blank");
-});
+// imageLink5.addEventListener("click", () => {
+//   window.open("winner.html", "_blank");
+// });
 
 // buy buttons
-const buyBook1 = document.getElementById("buy-5am");
-const buyBook2 = document.getElementById("buy-devil");
-const buyBook3 = document.getElementById("buy-rich");
-const buyBook4 = document.getElementById("buy-leaders");
-const buyBook5 = document.getElementById("buy-winners");
+// const buyBook1 = document.getElementById("buy-5am");
+// const buyBook2 = document.getElementById("buy-devil");
+// const buyBook3 = document.getElementById("buy-rich");
+// const buyBook4 = document.getElementById("buy-leaders");
+// const buyBook5 = document.getElementById("buy-winners");
 
 // buy button
-buyBook1.addEventListener("click", () => {
-  window.open("5amBook.html", "_blank");
-});
+// buyBook1.addEventListener("click", () => {
+//   window.open("5amBook.html", "_blank");
+// });
 
-buyBook2.addEventListener("click", () => {
-  window.open("devilBook.html", "_blank");
-});
+// buyBook2.addEventListener("click", () => {
+//   window.open("devilBook.html", "_blank");
+// });
 
-buyBook3.addEventListener("click", () => {
-  window.open("richDad.html", "_blank");
-});
+// buyBook3.addEventListener("click", () => {
+//   window.open("richDad.html", "_blank");
+// });
 
-buyBook4.addEventListener("click", () => {
-  window.open("leaders.html", "_blank");
-});
+// buyBook4.addEventListener("click", () => {
+//   window.open("leaders.html", "_blank");
+// });
 
-buyBook5.addEventListener("click", () => {
-  window.open("winner.html", "_blank");
-});
+// buyBook5.addEventListener("click", () => {
+//   window.open("winner.html", "_blank");
+// });
 
 
 //footer
-const gitLink = document.getElementById("git");
-const linkedInLink = document.getElementById("linkedin");
+// const gitLink = document.getElementById("git");
+// const linkedInLink = document.getElementById("linkedin");
 
-gitLink.addEventListener("click", () => {
-  window.open("https://github.com/aleks-plv", "_blank");
-});
+// gitLink.addEventListener("click", () => {
+//   window.open("https://github.com/aleks-plv", "_blank");
+// });
 
-linkedInLink.addEventListener("click", () => {
-  window.open("https://www.linkedin.com/in/aleksandrina-velikova-4b31bb275/", "_blank");
-});
+// linkedInLink.addEventListener("click", () => {
+//   window.open("https://www.linkedin.com/in/aleksandrina-velikova-4b31bb275/", "_blank");
+// });
